e4aaaf214fc16d915be65eb010f7da4c7f5a476b
ej <99721216+EjDadivas@users.noreply.github.com>
2023-10-09 17:46:34 +0800

    add function for saving and verifiying upload file

--- app/routers/upload_data.py
+++ app/routers/upload_data.py
@@ -1,9 +1,16 @@
-from fastapi import APIRouter
-from fastapi import UploadFile
-
+from fastapi import APIRouter, UploadFile, HTTPException
+import shutil
 router = APIRouter()
-
 
 @router.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
+    if not file.filename.endswith(('.csv', '.xlsx')):
+        raise HTTPException(status_code=400, detail="Invalid file type. Only .csv and .xlsx files are allowed.")
+
+    file_location = f"./uploaded_files/{file.filename}"
+    
+    with open(file_location, "wb+") as file_object:
+        shutil.copyfileobj(file.file, file_object)   
+         
+    return {"info": f"file '{file.filename}' saved at '{file_location}'"}
+    
