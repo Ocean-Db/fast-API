0b516778d6019fa3947306a4a64473ed6b6fd11d
Mikhail Pavlov <mikhail.pavlov@internet.ru>
2023-10-10 10:24:12 +0300

    Apply linting

--- app/routers/database.py
+++ app/routers/database.py
@@ -1,12 +1,12 @@
-from sqlalchemy import create_engine, URL
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import URL, create_engine
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 from . import schemas
 
-
-
 Base = declarative_base()
+
 
 def create_db_connection(credentials: schemas.DBCredentials):
 
@@ -18,13 +18,12 @@ def create_db_connection(credentials: schemas.DBCredentials):
         port=credentials.port,
         database=credentials.db_name,
     )
-    
+
     engine = create_engine(DATABASE_URL)
-    
+
     try:
         engine.connect()
-        SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+        SessionLocal = sessionmaker(
+            autocommit=False, autoflush=False, bind=engine)
     except SQLAlchemyError as se:
-        print("error", se.__cause__)
-
-
+        pass
