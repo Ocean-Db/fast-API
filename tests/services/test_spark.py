515f028c6d8366b75f2f2c375f5dee7cc97b9b1a
meaLuda <munyalamea@gmail.com>
2023-10-17 08:14:31 +0300

    changes to testfunction

--- tests/services/test_spark.py
+++ tests/services/test_spark.py
@@ -1,20 +1,15 @@
 # go three folders up
-import logging
 
 from ...app.services.spark_oceandb import FileLoader, SparkSession
 
 
 class TestLoadData:
-    def load_excel(file_path: str = './data/SaleData.xlsx'):
+    def test_load_excel(file_path: str = './data/SaleData.xlsx'):
         """
             # Test file loading functionality
             # Default dest file provided
         """
-        logging.info('Starting Spark Session!')
-        try:
-            spark = FileLoader(SparkSession())
-            # load excel:Returns dataframe and its schema
-            _, schema = spark.load_excel(file_path)
-            logging.info('Code Executed Successfully')
-        except Exception as e:
-            logging.warning(e)
+        spark = FileLoader(SparkSession())
+        # load excel:Returns dataframe and its schema
+        _, schema = spark.load_excel(file_path)
+        return schema
