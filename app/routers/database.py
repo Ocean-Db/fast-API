5653e5077dfc50dcfe553a9087abf18c89c50181
ej <99721216+EjDadivas@users.noreply.github.com>
2023-10-28 22:05:17 +0800

    added mongodb connection function

--- app/routers/database.py
+++ app/routers/database.py
@@ -1,9 +1,9 @@
+from pymongo import MongoClient
 from sqlalchemy import URL, create_engine
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
-from . import schemas
 
-Base = declarative_base()
+from . import schemas
 
 
 def create_db_connection(credentials: schemas.DBCredentials):
@@ -23,5 +23,16 @@ def create_db_connection(credentials: schemas.DBCredentials):
         engine.connect()
         SessionLocal = sessionmaker(
             autocommit=False, autoflush=False, bind=engine)
+        return engine, SessionLocal
     except SQLAlchemyError as se:
+        pass
+
+
+def create_mongodb_connection(credentials: schemas.DBCredentials):
+    MONGO_URI = f'{credentials.db_server}://{credentials.username}:{credentials.password}@{credentials.host}/{credentials.db_name}'
+    try:
+        client = MongoClient(MONGO_URI)
+        db = client.get_database(credentials.db_name)
+        return client
+    except Exception as e:
         pass
